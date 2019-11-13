d19cac5e37ddbd5826c628769138f082199d17c9
iam46479717 <arnaucr1997@gmail.com>
2019-11-13 18:38:19 +0100

    Segundo commit

--- src/people/Main.java
+++ src/people/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Person p1 = new Person(47,"Pepito","46479717Y");
         System.out.println(p1.toString());
+
+        System.out.println("Editando");
+        //Edit 1!
     }
 
 }
